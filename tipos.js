e919619279fc543d17f5c520f422add7a2bf3e81
gabrielf-fraga <gabrielfragaQI@gmail.com>
2023-07-12 14:16:41 -0300

    curso js

---
+++ tipos.js
@@ -0,0 +1,39 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Gabriel";
+
+
+
+console.log(citacao + meuNome)
+
+//type number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
+
+
+
